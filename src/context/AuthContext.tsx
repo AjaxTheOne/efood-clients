c523d892f5fb90a74f8bd6638aa72f8fa816005c
Lefteris Panelas <panelasajax@gmail.com>
2025-02-12 20:21:51 +0200

    User profile and addresses.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,46 +1,91 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { LoginResponse, User } from "../types/user";
+import { LoginResponse, RegisterCredentials, LoginCredentials, RegisterResponse, User } from "../types/user";
 import axiosInstance from "../api/axiosInstance";
+import { useNavigate } from "react-router";
 
 const AuthContext = createContext<{
-    user: User | undefined, 
+    user: User | null, 
     token: string | null,
-    login: (credentials: {email: string; password: string; }) => void
+    login: (credentials: LoginCredentials) => void,
+    register: (credentials: RegisterCredentials) => void,
+    logout: () => void
 }>({
-    user: undefined, 
+    user: null, 
     token: null, 
     login: () => null,
+    logout: () => null,
+    register: () => null
 });
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState<User>();
+    const localStorageUser = localStorage.getItem("user");
+    const [user, setUser] = useState<User | null>(
+        localStorageUser 
+            ? JSON.parse(localStorageUser) as User
+            : null
+    );
     const [token, setToken] = useState<string | null>(localStorage.getItem("token"));
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (token) {
             axiosInstance.get("/client/users/me")
                 .then((response) => {
-                    console.log(response.data);
                     setUser(response.data);
+                    localStorage.setItem("user", JSON.stringify(response.data));
                 });
         }
     }, []);
 
-    const login = ({email, password}: {email: string; password: string; }) => {
+    const login = ({email, password}: LoginCredentials) => {
         axiosInstance.post<LoginResponse>(
                 "/client/auth/login", 
                 {email, password}
             )
             .then((response) => {
+                if (!response.data.success) {
+                    return;
+                }
+
                 const data = response.data.data;
-                setUser(data.user);
-                setToken(data.token);
-                localStorage.setItem("token", data.token);
+                afterAuthentication(data);
             });
     };
 
+    const register = ({name, email, password}: RegisterCredentials) => {
+        axiosInstance.post<RegisterResponse>(
+                "/client/auth/register", 
+                {name, email, password}
+            )
+            .then((response) => {
+                if (!response.data.success) {
+                    return;
+                }
+
+                const data = response.data.data;
+                afterAuthentication(data);
+            });
+    };
+
+    const afterAuthentication = (data: RegisterResponse["data"] | LoginResponse["data"]) => {
+        setUser(data.user);
+        setToken(data.token);
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        
+        navigate("/");
+    }
+
+    const logout = () => {
+        setUser(null);
+        setToken(null);
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+    };
+
     return (
-        <AuthContext.Provider value={{ user, token, login }}>
+        <AuthContext.Provider value={{ user, token, login, register, logout }}>
             {children}
         </AuthContext.Provider>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,31 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import MainLayout from './layouts/MainLayout.tsx'
 import Home from './pages/Home.tsx'
 import Login from './pages/Login.tsx'
 import Register from './pages/Register.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
+import Profile from './pages/Profile.tsx'
+import ProtectedRoute from './component/helper/ProtectedRoute.tsx'
 
 createRoot(document.getElementById('root')!).render(
+	<BrowserRouter>
 		<AuthProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<MainLayout/>}>
-						<Route path='/' element={<Home/>}></Route>
-						<Route path='/login' element={<Login/>}></Route>
-						<Route path='/register' element={<Register/>}></Route>
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<Routes>
+				<Route element={<MainLayout/>}>
+					<Route path='/' element={<Home/>}></Route>
+
+					{/* Auth */}
+					<Route path='/login' element={<Login/>}/>
+					<Route path='/register' element={<Register/>}/>
+					<Route path='/profile' element={
+						<ProtectedRoute>
+							<Profile/>
+						</ProtectedRoute>
+					}/>		
+				</Route>
+			</Routes>
 		</AuthProvider>
+	</BrowserRouter>
 )
