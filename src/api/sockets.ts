cf6a27d38b27433ab280ea8417fcab744d904eca
Pagonoudis <giorgospago23@gmail.com>
2025-05-12 21:30:03 +0300

    Update socket connection URL to use production endpoint directly

--- src/api/sockets.ts
+++ src/api/sockets.ts
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.PROD 
-    ? "https://sockets.efood.pagonoudis.gr" 
-    : "http://localhost:3033";
+const URL = "https://sockets.efood.pagonoudis.gr";
+// const URL = import.meta.env.PROD 
+//     ? "https://sockets.efood.pagonoudis.gr" 
+//     : "http://localhost:3033";
 
 export const socket = io(URL);
